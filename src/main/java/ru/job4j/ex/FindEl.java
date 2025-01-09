22969ce1762355d2f4173e71d5264b81071c9a71
Home91 <jhon2002@mail.ru>
2025-01-09 19:35:29 +0300

    0.4. Пользовательские исключения. [#219367]

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,25 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int result = -1;
+        for (int index = 0; index < value.length; index++) {
+            if (value[index].equals(key)) {
+                result = index;
+            }
+        }
+        if (result == -1) {
+            throw new ElementNotFoundException("Элемент не найден");
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String[] value = {"one", "two", "three", "four"};
+        try {
+            indexOf(value, "five");
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
