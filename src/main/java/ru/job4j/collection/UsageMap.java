ee5800f7563487e5efe65d413f4806dce3ec0a8f
Home91 <jhon2002@mail.ru>
2025-01-20 08:32:51 +0300

    0. Map, HashMap [#212652]

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,13 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("jhon2002@mail.ru", "Evgeny Fedorovyh");
+        for (String key : map.keySet()) {
+            System.out.println(key + " = " + map.get(key));
+        }
+    }
+}
