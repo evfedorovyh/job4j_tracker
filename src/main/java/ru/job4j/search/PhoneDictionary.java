de6c25ca42bdc4c0f55d0c7e4278f9866b30af6b
evfedorovyh <jhon2002@mail.ru>
2025-01-28 01:20:12 +0300

    7. Функции высшего порядка [#24255]

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -1,6 +1,7 @@
 package ru.job4j.search;
 
 import java.util.ArrayList;
+import java.util.function.Predicate;
 
 public class PhoneDictionary {
     private ArrayList<Person> persons = new ArrayList<>();
@@ -9,16 +10,14 @@ public class PhoneDictionary {
         this.persons.add(person);
     }
 
-    /**
-     * Вернуть список всех пользователей, который содержат key в любых полях.
-     * @param key Ключ поиска.
-     * @return Список пользователей, которые прошли проверку.
-     */
     public ArrayList<Person> find(String key) {
+        Predicate<Person> combine = (person) -> person.getPhone().contains(key)
+                || person.getAddress().contains(key)
+                || person.getName().contains(key)
+                || person.getSurname().contains(key);
         ArrayList<Person> result = new ArrayList<>();
         for (Person person : persons) {
-            if (person.getPhone().contains(key) || person.getAddress().contains(key)
-            || person.getName().contains(key) || person.getSurname().contains(key)) {
+            if (combine.test(person)) {
                 result.add(person);
             }
         }
--- src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -14,6 +14,9 @@ class PhoneDictionaryTest {
         phones.add(
                 new Person("Petr", "Arsentev", "534872", "Bryansk")
         );
+        phones.add(
+                new Person("Ivan", "Petrov", "265984", "Moscow")
+        );
         ArrayList<Person> persons = phones.find("Petr");
         assertThat(persons.get(0).getSurname()).isEqualTo("Arsentev");
     }
@@ -24,7 +27,88 @@ class PhoneDictionaryTest {
         phones.add(
                 new Person("Petr", "Arsentev", "534872", "Bryansk")
         );
-        ArrayList<Person> persons = phones.find("Ivan");
+        phones.add(
+                new Person("Ivan", "Petrov", "265984", "Moscow")
+        );
+        ArrayList<Person> persons = phones.find("lex");
+        assertThat(persons.isEmpty()).isTrue();
+    }
+
+    @Test
+    public void whenFindBySurname() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        phones.add(
+                new Person("Ivan", "Petrov", "265984", "Moscow")
+        );
+        ArrayList<Person> persons = phones.find("tro");
+        assertThat(persons.get(0).getSurname()).isEqualTo("Petrov");
+    }
+
+    @Test
+    public void whenDontFindBySurname() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        phones.add(
+                new Person("Ivan", "Petrov", "265984", "Moscow")
+        );
+        ArrayList<Person> persons = phones.find("dor");
+        assertThat(persons.isEmpty()).isTrue();
+    }
+
+    @Test
+    public void whenFindByPhone() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        phones.add(
+                new Person("Ivan", "Petrov", "265984", "Moscow")
+        );
+        ArrayList<Person> persons = phones.find("65");
+        assertThat(persons.get(0).getSurname()).isEqualTo("Petrov");
+    }
+
+    @Test
+    public void whenDontFindByPhone() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        phones.add(
+                new Person("Ivan", "Petrov", "265984", "Moscow")
+        );
+        ArrayList<Person> persons = phones.find("111");
+        assertThat(persons.isEmpty()).isTrue();
+    }
+
+    @Test
+    public void whenFindByAddress() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        phones.add(
+                new Person("Ivan", "Petrov", "265984", "Moscow")
+        );
+        ArrayList<Person> persons = phones.find("yan");
+        assertThat(persons.get(0).getSurname()).isEqualTo("Arsentev");
+    }
+
+    @Test
+    public void whenDontFindByAddress() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        phones.add(
+                new Person("Ivan", "Petrov", "265984", "Moscow")
+        );
+        ArrayList<Person> persons = phones.find("burg");
         assertThat(persons.isEmpty()).isTrue();
     }
 }
